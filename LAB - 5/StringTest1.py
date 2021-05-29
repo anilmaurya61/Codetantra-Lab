2e76428cb65a9eca16425addac2b4855bde09a93
anilmaurya61 <anilkumarmaurya246@gmail.com>
2021-05-29 20:20:57 +0530

    updated

---
+++ LAB - 5/StringTest1.py
@@ -0,0 +1,8 @@
+# Note: Python and python are different.
+s=input("Enter a string: ")
+s1="Python"
+s2=s[:7]
+if s1==s2:
+	print("The given string is: "+s)
+else:
+	print("The string after adding 'Python' is: "+s1+" "+s)
