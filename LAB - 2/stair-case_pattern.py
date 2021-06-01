a1370250af2c32284ac974c91ad341bc42cec3f8
Anil maurya <anilkumarmaurya246@gmail.com>
2021-06-01 11:09:16 +0530

    Update stair-case_pattern.py

---
+++ LAB - 2/stair-case_pattern.py
@@ -0,0 +1,15 @@
+def pattern(n):
+	for i in range(1,n+1):
+		k=i+1 if(i%2!=0) else i
+		for g in range(k,n):
+			if g>=k:
+				print(end="  ")
+			
+		for j in range(0,k):
+			if j == k - 1:
+				print(" * ")
+			else:
+				print(" * ",end =" ")
+				
+a=int(input("Enter the number of steps to be printed: "))
+pattern(a)
