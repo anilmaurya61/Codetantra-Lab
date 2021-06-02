5668b573f13288e90b91ae8fb1e4b831dd52c9e1
anilmaurya61 <anilkumarmaurya246@gmail.com>
2021-06-02 12:14:41 +0530

    update

---
+++ JAVA Lab/primitiveTypes.java
@@ -0,0 +1,21 @@
+package q10815;
+public class PrimitiveTypes{
+	public static void main(String[] args){
+		
+		byte b=0;
+		short s=0;
+		int i=0;
+		long l=0;
+		boolean b1=false;
+		double d=0.0d;
+		float f=0.0f;
+		
+		System.out.println("byte default value = "+b);
+		System.out.println("short default value = "+s);
+		System.out.println("int default value = "+i);
+		System.out.println("long default value = "+l);
+		System.out.println("boolean default value = "+b1);
+		System.out.println("double default value = "+d);
+		System.out.println("float default value = "+f);
+	}
+}
