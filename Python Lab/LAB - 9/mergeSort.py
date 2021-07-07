34afc0f94d8d4e3f879243b0448b5f233f6c3308
akansha1210 <74556894+akansha1210@users.noreply.github.com>
2021-07-07 11:36:43 +0530

    Create mergeSort.py

---
+++ Python Lab/LAB - 9/mergeSort.py
@@ -0,0 +1,36 @@
+def mergeSort(nlist):
+	print("Splitting ",nlist)
+	if(len(nlist)>1):
+		mid = len(nlist) // 2
+		lefthalf = nlist[:mid]
+		righthalf = nlist[mid:]
+		
+		mergeSort(lefthalf)		
+		mergeSort(righthalf)
+		i=j=k=0
+		
+		while i < len(lefthalf) and j < len(righthalf):
+			if lefthalf[i] < righthalf[j]:
+				nlist[k] = lefthalf[i]
+				i = i + 1
+			else:
+				nlist[k] = righthalf[j]
+				j = j + 1
+			k = k + 1
+		while i < len(lefthalf) :
+			nlist[k] = lefthalf[i]		
+			i = i + 1	
+			k = k + 1	
+		while j < len(righthalf):		
+			nlist[k] = righthalf[j]		
+			j = j + 1		
+			k = k + 1	
+	print("Merging ",nlist)
+ 	
+a=str(input("Enter the list of numbers: "))
+arr = a.split(" ")
+for i in arr:
+	i = int(i)
+n = len(arr)
+mergeSort(arr)
+print(arr)
